cfc99faaefa505996f872bae641d8c7d685d7f1d
Peter Feichtinger <shippo@gmx.at>
2017-05-13 18:20:40 +0200

    Add code examples for the Spartanizer

---
+++ draftpaper/code/C1.java
@@ -0,0 +1,19 @@
+public class C1<T> {
+	private final T inner;
+	public C1(T inner) {
+		this.inner = inner;
+	}
+	public int hashCode() {
+		return 31 +
+				((inner == null) ? 0 : inner.hashCode());
+	}
+	public boolean equals(Object c) {
+		return c == this ||
+				c != null && getClass() == c.getClass() &&
+				equals((C1) c);
+	}
+	private boolean equals(C1 c) {
+		return inner == null ?
+				c.inner == null : inner.equals(c.inner);
+	}
+}
